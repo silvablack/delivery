66b39cd2c53d0fdfa1cd9bd5aa59a7072056656c
Paulo Silva <paulosilvadev3@gmail.com>
2019-02-04 16:11:49 -0300

    api and register user with api token

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('register', 'Auth\RegisterController@register');
+
 Route::get('foods', 'FoodsController@getAll');
 
 Route::get('foods/{id}', 'FoodsController@getOne');
